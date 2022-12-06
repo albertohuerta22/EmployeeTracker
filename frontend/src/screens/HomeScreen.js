f0ccfcb17df808b978424035dcadfa12efea84cb
Alberto Huerta <alberto.huerta@kaseya.com>
2022-12-06 10:02:18 +0000

    created form container component and homescreen login page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,32 @@
 import React from 'react';
-
+import FormContainer from '../components/FormContainer';
+import { Form } from 'react-bootstrap';
 const HomeScreen = () => {
-  return <div>This is my HomeScreen</div>;
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="username"
+            placeholder="Enter Username"
+            value=""
+            //onchange listner
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value=""
+            //onchange
+          ></Form.Control>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default HomeScreen;
